523e3ed04664baaffc7a2f1e50e83f2e38df3c62
khoirotunisa16 <khoirotun976@gmail.com>
2021-02-17 11:25:07 +0700

    add new files

--- maudycinta/application/controllers/crud.php
+++ maudycinta/application/controllers/crud.php
@@ -18,7 +18,7 @@ class Crud extends CI_Controller{
 	function tambah(){
 		$this->load->view('v_input');
 	}
- 
+
 	function tambah_aksi(){
 		$nama = $this->input->post('nama');
 		$alamat = $this->input->post('alamat');
@@ -29,8 +29,36 @@ class Crud extends CI_Controller{
 			'alamat' => $alamat,
 			'pekerjaan' => $pekerjaan
 			);
-		$this->m_data->input_data($data,'user');
+		$this->m_data->input_data($data,'users');
 		redirect('crud/index');
 	}
- 
+	function edit($id){
+		$where = array('id' => $id);
+		$data['user'] = $this->m_data->edit_data($where,'users')->result();
+		$this->load->view('v_edit',$data);
+	}
+	function update(){
+		$id = $this->input->post('id');
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+	 
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+		);
+	 
+		$where = array(
+			'id' => $id
+		);
+	 
+		$this->m_data->update_data($where,$data,'users');
+		redirect('crud/index');
+	}
+	function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'users');
+		redirect('crud/index');
+	}
 }
--- maudycinta/application/views/v_input.php
+++ maudycinta/application/views/v_input.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</title>
+	<title>Membuat CRUD dengan CodeIgniter | Maudycinta</title>
 </head>
 <body>
 	<center>
-		<h1>Membuat CRUD dengan CodeIgniter | MalasNgoding.com</h1>
+		<h1>Membuat CRUD dengan CodeIgniter | Maudycinta</h1>
 		<h3>Tambah data baru</h3>
 	</center>
 	<form action="<?php echo base_url(). 'crud/tambah_aksi'; ?>" method="post">
